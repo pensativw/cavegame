856c168d325b7c9a2c8019f2f84b79930f793859
Maurício <mauri.ms@outlook.com>
2023-09-06 22:12:43 -0300

    v0.0.2.4

--- start.py
+++ start.py
@@ -1,10 +1,16 @@
 ##  Estou aprendendo a usar o Python criando esse jogo em linha de comando.
 ##  Críticas construtivas são aceitas.
+import time
 
 input('\nPressione Enter para jogar Cave Game.')
 
+input('\nAntes de tudo, qual é o seu nome?') #incompleto
 
-input('\nVocê acordou numa caverna...')
+x = range(3, 6)
+for n in x:
+    print(n)
+    time.sleep(3)
+input('\nVocê acabou de acordar')
 input('\nTu mal consegue enxergar...')
 input('\nO que você faz?')
 
@@ -13,10 +19,17 @@ while True:
     while True:
         A02=False
         try:
-            A01=int(input('\n1. Grito por ajuda\n2. Se levantar\n\n-> '))
-            if A01==1: input('\nNada acontece.')
+            A01=int(input('\n1. Gritar por ajuda\n2. Se levantar\n\n-> '))
+            if A01==1:
+                print('\nSOCORRO!!!')
+                time.sleep(3)
+                print('\nALGUÉM AÍ!!!!??')
+                time.sleep(3)
+                input('\nNada acontece.')
+                A00=True
             elif A01==2:
-                input('\nPedras desabaram depois que você se levantou...')
+                input('\nTu percebe que está num buraco, e sai daquele buraco.')
+                input('\nE dá graças a Deus que ao sair você não foi esmagado por rochas que acabaram de ser empurradas por alguém...')
                 A02=True
                 break
             else:
@@ -34,7 +47,7 @@ while True:
         f00=input('\nEle é o... -> ')
         
         if f00=='':
-            input('\nEscreva o seu nome.')
+            input('\nEscreva o nome do seu amigo.')
             continue
         else: break
     input(f'\nO {f00} está desacordado. Você tem 3 opções...')
@@ -45,7 +58,7 @@ while True:
             AB02=False #    Acordar
             AB03=False #    Abandono
             try:
-                AB00=int(input('\n1. Comer de sua carne, porque você está drogado\n2. O acordar\n3. Deixá-lo desacordado e seguir em frente\n\n-> '))
+                AB00=int(input('\n1. Comer de sua carne, por algum motivo\n2. O acordar\n3. Deixá-lo desacordado e seguir em frente\n\n-> '))
                 if AB00==1:
                     AB01=True
                     AB02=True
@@ -83,7 +96,7 @@ while True:
                             break
         
         elif AB02==True:
-            input(f'Com muito sono, {f00} acorda e tenta lembrar a você o que aconteceu na noite passada.')
+            input(f'Com muito sono, {f00} acorda e tenta lembrar do que houve na noite passada.')
             input(' ')
         
         elif AB03==True: #  Abandonado
@@ -99,15 +112,24 @@ while True:
         
             if AC01==False:
                 input('\nTu sabe como a gente chegou aqui? Eu não me lembro.')
-                while True:
-                    try:
-                        input('\n1. Não\n2. Mentir')
-                    except ValueError:
-                        input('\nEscreva um número.')
+
             elif AC01==True:
                 input("\nTU  LEMBRA COMO QUE A GENTE CHEGOU AQUI? PELO MENOS TU SABE DISSO, NUM É?")
-    
-    else:input('\nVocê é um belo de um filha da puta.')
+            
+            elif AC02==True:
+                input('\nBELEZA, BELEZA! O QUE A GENTE FAZ AGORA? VOU TE COMER SE TU NÃO ME DER NENHUMA SUGESTÃO >:(')
+            while True:
+                try:
+                    input('\n1. Como como')
+                except ValueError:
+                    input('\nEscreva um número')
+                    
+    elif AB03==True and A00==True:
+        input('EI!')
+        time.sleep(2)
+        input('Você ')
+        while True:
+            input('1. Não')
     
     break
 input('\nEsse foi o Cave Game! Espero que tenha gostado!')
